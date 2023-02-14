d93f8ac13571ce7e163f92e6acea16cbc6b6e28b
Mindy Ng <mindyng85@gmail.com>
2023-02-13 16:13:46 -0800

    Update query.sql

--- 1_Dannys_Diner/query.sql
+++ 1_Dannys_Diner/query.sql
@@ -26,7 +26,45 @@ GROUP BY 1
 ORDER BY 2 DESC;
 
 -- 3. What was the first item from the menu purchased by each customer?
+
+--window fxn to rank order_date in ASC order by each customer
+--filter on first order date
+--output first order per customer
+
+WITH orders AS (
+SELECT customer_id
+  , product_id
+, ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS order_rk
+FROM dannys_diner.sales
+)
+
+SELECT customer_id, product_name
+FROM orders
+JOIN dannys_diner.menu
+ON orders.product_id = menu.product_id
+WHERE order_rk = 1;
+
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+SELECT customer_id
+, COUNT(sales.product_id) AS popular_item_cnt
+FROM dannys_diner.sales
+
+
+JOIN
+
+(
+SELECT product_id
+, COUNT(*) AS item_count
+FROM dannys_diner.sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1
+) AS most_popular_item
+ON sales.product_id = most_popular_item.product_id
+GROUP BY 1
+;
+
 -- 5. Which item was the most popular for each customer?
 -- 6. Which item was purchased first by the customer after they became a member?
 -- 7. Which item was purchased just before the customer became a member?
