5b558115121dd777cbdea26dc1f5ea66dc9b3a64
Mindy Ng <mindyng85@gmail.com>
2023-02-13 16:46:48 -0800

    Update query.sql

--- 1_Dannys_Diner/query.sql
+++ 1_Dannys_Diner/query.sql
@@ -48,8 +48,8 @@ WHERE order_rk = 1;
 
 SELECT customer_id
 , COUNT(sales.product_id) AS popular_item_cnt
-FROM dannys_diner.sales
 
+FROM dannys_diner.sales
 
 JOIN
 
@@ -61,12 +61,57 @@ GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 1
 ) AS most_popular_item
+
 ON sales.product_id = most_popular_item.product_id
 GROUP BY 1
 ;
 
 -- 5. Which item was the most popular for each customer?
+
+WITH cte AS (
+SELECT customer_id
+, product_id
+, COUNT(product_id) AS item_count
+FROM dannys_diner.sales
+GROUP BY 1
+, 2
+)
+
+, item_pop_rk AS (
+SELECT customer_id
+, product_id
+, item_count
+, DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY item_count DESC) AS item_rk
+FROM cte
+)
+
+SELECT customer_id
+, product_id
+FROM item_pop_rk
+WHERE item_rk = 1;
+
 -- 6. Which item was purchased first by the customer after they became a member?
+
+--join customer orders with members based on cust_id AND orders made after sign up date
+--order orders by earliest date per customer
+--filter on first date (closest to join date)
+--output item purchased
+
+WITH cte AS (
+SELECT sales.customer_id
+, sales.product_id
+, DENSE_RANK() OVER (PARTITION BY sales.customer_id ORDER BY sales.order_date) AS order_date_rk
+FROM dannys_diner.sales
+JOIN dannys_diner.members
+ON sales.customer_id = members.customer_id
+AND sales.order_date > members.join_date
+)
+
+SELECT customer_id
+, product_id AS first_purchase_after_membership
+FROM cte
+WHERE order_date_rk = 1
+
 -- 7. Which item was purchased just before the customer became a member?
 -- 8. What is the total items and amount spent for each member before they became a member?
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
